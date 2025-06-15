f58d0f9dd54328472951baa72727dcdb1c8bcd50
Ujjwal Mishra <ujjwalmishra9936@gmail.com>
2025-06-15 18:36:54 +0530

    ready for production2

--- MyHealthMate/settings.py
+++ MyHealthMate/settings.py
@@ -26,14 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-ENVIRONMENT = 'development'  # Change to 'production' in production environment
+ENVIRONMENT = 'production'  # Change to 'production' in production environment
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if ENVIRONMENT == 'development' else False
 
 ALLOWED_HOSTS = ['192.168.165.139', 'localhost','127.0.0.1','myhealthmate.up.railway.app']
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',  # For local dev
-    'https://d530-2409-4089-be11-bdca-177-5267-2a17-7bfa.ngrok-free.app'
+    'https://d530-2409-4089-be11-bdca-177-5267-2a17-7bfa.ngrok-free.app',
     'https://myhealthmate.up.railway.app',
 ]
 
